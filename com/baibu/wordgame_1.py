0423ddabcfdf3246f391e160365c26a8e6dc1128
Yaodong Zhang <yaodong199@icloud.com>
2021-04-22 23:10:44 +0800

    Initial commit

---
+++ com/baibu/wordgame_1.py
@@ -0,0 +1,9 @@
+print("welcome to my wordgame")
+temp = input("guess what numbers in the heart")
+guess = int(temp)
+if guess == 8:
+	print("right")
+	print("there is nothing score");
+else:
+	print("false, in my heart is 8!");
+print("game over!")
