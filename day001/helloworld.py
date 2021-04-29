de71d4aeb509c091be63bcfd643efd43d709db6b
Yaodong Zhang <yaodong199@icloud.com>
2021-04-29 23:09:51 +0800

    Update helloworld.py

--- day001/helloworld.py
+++ day001/helloworld.py
@@ -5,3 +5,12 @@ print("hello world!","hello world")
 
 # 转义字符
 print("hello\nworld!")
+
+#
+world="hello world!"
+print(world)
+print('id',id(world))
+
+print('type',type(world))
+
+print('value',world)
