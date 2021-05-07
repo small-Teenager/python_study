acbc370aee18fe20be54f8222f15d7945310232c
Yaodong Zhang <yaodong199@icloud.com>
2021-05-08 00:20:02 +0800

    add 参数赋值

--- day006/symbol.py
+++ day006/symbol.py
@@ -11,3 +11,41 @@ print(2**3) # 2的3次方
 print(-11//2)
 print(11//-2)
 print(-11//-2)
+
+# 赋值运算符
+a=3+4
+print(a)
+a=b=c=20 # 链式赋值
+print(a,id(a))
+print(b,id(b))
+print(c,id(c))
+
+# 参数赋值
+a=20
+print(a,id(a))
+a+=30
+print(a,id(a))
+# 解包赋值
+a,b,c=2,3,4
+print(a,b,c,id(a),id(b),id(c),type(a),type(b),type(c))
+
+# swap 解包赋值
+a,b=7,8
+# 交换前
+print(a,b,id(a),id(b),type(a),type(b))
+# 交换后
+a,b=b,a
+print(a,b,id(a),id(b),type(a),type(b))
+
+
+
+
+
+
+
+
+
+
+
+
+
