5a0371744d64dd6d3f069ebd3dfbc9178d88e12d
Yaodong Zhang <yaodong199@icloud.com>
2021-05-21 17:47:59 +0800

    Create object.py

---
+++ day024/object.py
@@ -0,0 +1,39 @@
+class Student:
+    native_pace = 'china'
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print("调用父类构造函数")
+
+    def eat(self):
+        print('吃饭')
+
+    def play(self):
+        print('play')
+
+    # 静态方法
+    @staticmethod
+    def staticMethod():
+        print('这是一个静态方法')
+
+    @classmethod
+    def classMethod(cls):
+        print('这是一个类方法')
+
+
+print(type(Student), id(Student), Student)
+
+Student.staticMethod()
+
+stu = Student('张飞', 18)
+
+stu.eat()
+stu.play()
+
+# 类属性调用
+print(Student.native_pace)
+
+Student.native_pace = 'mainland'
+
+print(Student.native_pace)
